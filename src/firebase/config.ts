08f12cdfe3ef4bb7de27186b688c7c9fb324ffde
Good Doctor <doctorgood119@gmail.com>
2026-04-01 07:15:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2010803707-7f750",
+  "appId": "1:462224879560:web:a914e2ad7d406babad1afd",
+  "apiKey": "AIzaSyCJoQmlORJF6AIEJjJZaZ5mf-8SAlmxXz0",
+  "authDomain": "studio-2010803707-7f750.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "462224879560"
+};
